2fa906eebdd9d2519a21d0721b85e50bfb74b342
ahb <andreas.bihlmaier@gmx.de>
2014-08-04 21:29:03 +0200

    KBs are not very consistend (e.g. BEGIN FILE); cope with it

--- python/ontoprog.py
+++ python/ontoprog.py
@@ -7,24 +7,28 @@ import re
 import os
 import argparse
 import pyparsing
+import collections
 
 
 
 
 class SUOKIFEntity(object):
-  def __init__(self, name):
+  def __init__(self, name, parents = []):
     self.name = name
+    self.parents = parents
 
   def plot(self, graph):
     graph.add_node(self.name)
+    for parent in self.parents:
+      graph.add_edge(self.name, parent, label='parent')
 
 
 
 
 class SUOKIFPart(object):
-  def __init__(self, **kwargs):
-    self.entities = []
-    self.name = ''
+  def __init__(self, name, **kwargs):
+    self.entities = {}
+    self.name = name
     self.name_re = re.compile(r'^;;\s+?(.*)\s+?;;$')
 
     if 'string' in kwargs:
@@ -32,26 +36,30 @@ class SUOKIFPart(object):
 
 
   def plot(self, graph):
-    graph.add_subgraph(self.entities, 'cluster_' + self.name, color='gray', label=self.name)
-    for entity in self.entities:
-      entity.plot(graph)
+    graph.add_subgraph(self.entities.keys(), 'cluster_' + self.name, color='gray', label=self.name)
+    for entity_name in self.entities:
+      self.entities[entity_name].plot(graph)
 
 
   def from_string(self, string):
     lines = string.split('\n')
     lines = [line.strip() for line in lines]
-    lines = self.consume_name(lines)
+    self.extract_name(lines)
     lines = [line.strip() for line in lines if line and not line.startswith(';')]
     while lines:
       lines = self.consume_item(lines)
 
 
-  def consume_name(self, lines):
+  def extract_name(self, lines):
     for (line_index, line) in enumerate(lines):
-      match = self.name_re.match(line)
-      if match != None:
-        self.name = match.group(1).strip()
-        break
+      if line_index >= 1 and lines[line_index - 1].startswith(';;;;') and lines[line_index + 1].startswith(';;;;'):
+        match = self.name_re.match(line)
+        if match != None:
+          text = match.group(1).strip()
+          if not text.startswith('Start:'):
+            self.name = text
+            break
+    print('part: %s' % self.name)
     return lines[line_index+1:]
 
 
@@ -79,9 +87,24 @@ class SUOKIFPart(object):
 
 
   def parse_item(self, string):
-    print('string:', string)
-    parsed_string = pyparsing.OneOrMore(pyparsing.nestedExpr()).parseString(string)
-    print('parsed:', parsed_string)
+    #print('string:', string)
+    parsed_string = pyparsing.nestedExpr().parseString(string)
+    #print('parsed:', parsed_string)
+    fun, args = parsed_string[0][0], parsed_string[0][1:]
+    #print('fun: %s args: %s' % (fun, args))
+    if fun == 'subclass':
+      self.add_subclass(args[0], args[1])
+
+
+  def add_subclass(self, child, parent):
+    if isinstance(parent, str): # parent is either a str or a sequence of str
+      parent_list = [parent]
+    else:
+      parent_list = [p for p in parent]
+    if child in self.entities:
+        self.entities[child].parents.extend(parent_list)
+    else:
+      self.entities[child] = SUOKIFEntity(child, parent_list)
 
 
 
@@ -93,7 +116,7 @@ class SUOKIFParser(object):
 
 
   def parse(self, kifs):
-    for kif in kifs:
+    for (kif_index, kif) in enumerate(kifs):
       kif_file = open(kif, 'r')
       kif_parts = []
       kif_part = ''
@@ -103,8 +126,11 @@ class SUOKIFParser(object):
         kif_part += kif_line
         if kif_line.startswith(';; END FILE'):
           kif_parts.append(kif_part)
-      for kif_part in kif_parts:
-        self.ontology_parts.append(SUOKIFPart(string=kif_part))
+      if not kif_parts: # kif file does not contain 'subfiles'
+        kif_parts.append(kif_part)
+      kif_file.close()
+      for (kif_part_index, kif_part) in enumerate(kif_parts):
+        self.ontology_parts.append(SUOKIFPart(os.path.basename(kif) + '_' + str(kif_part_index), string=kif_part))
 
 
   def plot_to_file(self, plot_filename):
@@ -130,7 +156,7 @@ def main():
 
   parser = SUOKIFParser()
   parser.parse(args.kifs)
-  parser.plot_to_file('/tmp/kifs.png')
+  parser.plot_to_file('/tmp/kifs.pdf')
 
 
 if __name__ == '__main__':
